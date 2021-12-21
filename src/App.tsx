00e39277ab11e863ce53cfaafffce1f490098226
yuriaguiar-86 <kakacraft337@gmail.com>
2021-12-21 18:14:07 -0300

    Anexar a img no firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState, useEffect, FormEvent } from 'react';
+
+import * as C from './App.styles';
+import * as P from './services/photos';
+import { Photo } from './types/photo';
+import { PhotoItem } from './components/PhotoItem';
+
+const App = () => {
+  const [upload, setUpload] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await P.getAll());
+      setLoading(false);
+    }
+
+    getPhotos();
+  }, []);
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+
+    if(file && file.size > 0){
+      setUpload(true);
+      let result = await P.insert(file);
+      setUpload(false);
+
+      if(result instanceof Error){
+        alert(`${result.name} - ${result.message}`);
+      }else{
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
+          <input type="file" name="image" />
+          <input type="submit" name="enviar" />
+          {upload && "Enviando..."}
+        </C.UploadForm>
+
+        {loading &&
+          <C.ScreenWarning>
+            <div className="emoji">🖐</div>
+            <div>Aguarde um momento...</div>
+          </C.ScreenWarning>
+        }
+
+        {!loading && photos.length > 0 &&
+          <C.PhotoList>
+            {photos.map((item, index) => (
+              <PhotoItem key={index} url={item.url} name={item.name} />
+            ))}
+          </C.PhotoList>
+        }
+
+        {!loading && photos.length === 0 &&
+          <C.ScreenWarning>
+            <div className="emoji">😔</div>
+            <div>Não há fotos cadastradas</div>
+          </C.ScreenWarning>
+        }
+      </C.Area>
+    </C.Container>
   );
 }
 
---
+++ src/services/photos.ts
@@ -0,0 +1,39 @@
+
+import { v4 as createId } from 'uuid';
+import { ref, listAll, getDownloadURL, uploadBytes } from 'firebase/storage';
+
+import { storage } from '../libs/firebase';
+import { Photo } from '../types/photo';
+
+export const getAll = async() => {
+    let list: Photo[] = [];
+
+    const imagesFolder = ref(storage, "images");
+    const photoList = await listAll(imagesFolder);
+    
+    for(let i in photoList.items) {
+        let photoUrl = await getDownloadURL(photoList.items[i]);
+
+        list.push({
+            name: photoList.items[i].name,
+            url: photoUrl
+        })
+    }
+
+    return list;
+}
+
+export const insert = async(file: File) => {
+    if(['image/jpeg', 'image/jpg', 'image/png'].includes(file.type)){
+
+        let randomName = createId();
+        let newFile = ref(storage, `images/${randomName}`);
+        let upload = await uploadBytes(newFile, file);
+        let photoUrl = await getDownloadURL(upload.ref);
+        
+        return { name: upload.ref.name, url: photoUrl } as Photo;
+
+    }else{
+        return new Error(`Tipo de arquivo não permitido`);
+    }
+}
